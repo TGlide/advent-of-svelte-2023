6433006e3c0bbf9252471ae0291d57eff20ba036
tglide <26071571+TGlide@users.noreply.github.com>
2023-12-09 21:50:29 +0000

    deeds WIP

--- src/routes/day/1/+page.svelte
+++ src/routes/day/1/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	import { initialData } from './data';
+	import { initialData, type Entry } from './data';
 
+	// State
 	let data = $state(initialData);
 	let adding = $state(false);
 
+	// Event Handlers
 	const onsubmit = (e: Event) => {
 		e.preventDefault();
 
@@ -11,9 +13,107 @@
 		const name = formData.get('name') as string;
 		const tally = Number(formData.get('tally'));
 
-		data.push({ name, tally });
+		data.unshift({ name, tally });
 		adding = false;
 	};
+
+	// Helpers
+	function randomPick<T>(arr: T[]) {
+		return arr[Math.floor(Math.random() * arr.length)];
+	}
+
+	function fireDeed(node: HTMLElement, deed: string) {
+		// Fires off a text from the node to a random direction. Shouldn't move too far, and should fade out.
+		const { x, y } = node.getBoundingClientRect();
+		const { width, height } = node.getBoundingClientRect();
+
+		const deedNode = document.createElement('div');
+		console.log(deedNode);
+		deedNode.classList.add(
+			...'absolute text-12 text-center max-w-20ch font-bold font-sans color-yellow-5'.split(' ')
+		);
+		deedNode.textContent = deed;
+
+		deedNode.style.left = `${x + width / 2}px`;
+		deedNode.style.top = `${y + height / 2}px`;
+
+		document.body.appendChild(deedNode);
+
+		const direction = Math.random() * 360;
+		const distance = Math.random() * 100 + 50;
+
+		const x2 = Math.cos(direction) * distance;
+		const y2 = Math.sin(direction) * distance;
+
+		const anim = deedNode.animate(
+			[
+				{
+					transform: `translate(${x2}px, ${y2}px)`,
+					opacity: 1,
+					offset: 0
+				},
+
+				{
+					transform: `translate(${x2}px, ${y2}px)`,
+					opacity: 1,
+					offset: 0.75
+				},
+
+				{
+					transform: `translate(${x2}px, ${y2}px)`,
+					opacity: 0
+				}
+			],
+			{
+				duration: 1000,
+				easing: 'ease-in-out',
+				fill: 'forwards'
+			}
+		);
+
+		anim.onfinish = () => {
+			deedNode.remove();
+		};
+	}
+
+	// Constants
+	const deeds = {
+		good: [
+			'Helped a grandma cross the street',
+			'Baked delicious cookies for the neighbors',
+			'Passed NNN',
+			'Helped a friend with their homework'
+		],
+		bad: [
+			"Slept with their friend's mom",
+			'Convinced someone to learn React',
+			'Called a basic opinion a hot take',
+			'Paid for Twitter Blue'
+		]
+	};
+
+	// Actions
+
+	type DeedArgs = {
+		type: 'good' | 'bad';
+		entry: Entry;
+	};
+
+	function deed(node: HTMLElement, { type, entry }: DeedArgs) {
+		const onclick = () => {
+			fireDeed(node, randomPick(deeds[type]));
+			if (type === 'good') entry.tally++;
+			else entry.tally--;
+		};
+
+		node.addEventListener('click', onclick);
+
+		return {
+			destroy() {
+				node.removeEventListener('click', onclick);
+			}
+		};
+	}
 </script>
 
 <div class="grid w-[calc(100vw-1rem)] max-w-600px mx-auto">
@@ -25,7 +125,7 @@
 			on:click={() => {
 				adding = !adding;
 			}}
-			class="i-solar-add-circle-bold text-24
+			class="{adding ? 'i-solar-minus-circle-bold' : 'i-solar-add-circle-bold'} text-24
 			hover:opacity-75 active:opacity-50 mis-auto"
 		/>
 	</div>
@@ -67,8 +167,8 @@
 				<span>{entry.name}</span>
 				<span class="{isNice ? 'color-green-5' : 'color-red-5'} font-bold">{entry.tally}</span>
 				<div class="flex gap-8">
-					<button onclick={() => entry.tally++}>Add</button>
-					<button onclick={() => entry.tally--}>Sub</button>
+					<button use:deed={{ entry, type: 'good' }}>Add</button>
+					<button use:deed={{ entry, type: 'bad' }}>Sub</button>
 					<button
 						on:click={() => {
 							data = data.filter((_, j) => i !== j);
